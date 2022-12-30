5c9cf140b8af105a5a9e843c9a73845a9b47ea54
ramzihaddad12 <ramzi.haddad12@gmail.com>
2022-12-30 14:53:04 +0200

    third draft

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -11,23 +11,20 @@ class Aboutme extends Component{
                     I acknowledge that this is somewhat optimistic/unrealistic, but I try to view life as a non-zero-sum game, 
                     where everyone can “win”. To me, humility is an essential component to “winning”, and therefore, I always attempt
                     to be, as Dijkstra puts it, a <a href = "https://www.cs.utexas.edu/users/EWD/transcriptions/EWD03xx/EWD340.html" >“Very Humble Programmer”</a>.
-                    
+                    <br/>
                     Here’s where I stand on some of the age-old arguments amongst programmers:
                     <ul style = {{fontSize: '35px', paddingLeft: '5em', paddingBottom: '10%', paddingRight: '2em', lineHeight: '1'}}>
                         <li>Vim over Emacs</li>
                         <li>Tabs over spaces</li>
                         <li>Linux over everything else</li>
                     </ul>
+                    <br/>
                     The favorites: 
                     <ul style = {{fontSize: '35px', paddingLeft: '5em', paddingBottom: '10%', paddingRight: '2em', lineHeight: '1'}}>
-                    <li>Favorite number: 1729, the smallest positive number that is the sum of 2 cubes in 2 different ways: (9, 10) and (1, 12). This number, referred to as a taxicab number, has a fascinating <a href = "https://mathworld.wolfram.com/Hardy-RamanujanNumber.html">story </a> 
-                        involving the mathematics legend that is Ramanujan.</li>
+                    <li>Favorite number: 1729, the smallest positive number that is the sum of 2 cubes in 2 different ways: (9, 10) and (1, 12). This number, referred to as a taxicab number, has a fascinating <a href = "https://mathworld.wolfram.com/Hardy-RamanujanNumber.html">story</a> 
+                         involving the mathematics legend that is Ramanujan.</li>
                     <br/>
                     <li>Favorite data structure: A Bloom filter. Although it’s not a deterministic structure, the <a href = "https://brilliant.org/wiki/bloom-filter/">theory</a> behind a Bloom filter seems pretty cool to me.</li>
-                    <br/>
-                    <li></li>
-                    <br/>
-                    <li></li>
                     <br/>
                     <li>Favorite random story: A very interesting <a href = "http://calteches.library.caltech.edu/4106/1/Samos.pdf">method</a> of how ancient Greeks managed to dig a tunnel through a mountain using right triangles in the 6th century B.C.</li>
                     <br/>
@@ -36,7 +33,7 @@ class Aboutme extends Component{
                     <li></li>
                     </ul>
                         
-                    <br/><br/>
+                    <br/>
                     In my free time, I enjoy devoting time to football (real football not american football), chess, reading about history, and Alfredo's pizza cafe.</p>
                 </div>
             </div>
